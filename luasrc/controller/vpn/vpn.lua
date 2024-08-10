3fb2b07e14bc4f6089e0b221eec2300f57b741ce
Maksim Kuklin <makskuk@gmail.com>
2024-08-11 02:06:56 +0500

    server-side api

--- luasrc/controller/vpn/vpn.lua
+++ luasrc/controller/vpn/vpn.lua
@@ -3,13 +3,16 @@
 
 module("luci.controller.vpn.vpn", package.seeall)
 
+local http = require "luci.http"
+local uci = require "luci.model.uci".cursor()
+
 function index()
 	-- if not nixio.fs.access("/etc/config/uhttpd") then
 	-- 	return
 	-- end
-	entry({"admin", "services", "pptp"}, cbi("vpn/pptp"), _("PPTP Client"), 30)
-	entry({"admin", "services", "pptp", "action"}, call("do_pptp_action"), nil).leaf = true
-
+	entry({"admin", "services", "vpn", "action"}, call("do_action"), nil).leaf = true
+	
+	entry({"admin", "services", "pptp"},   cbi("vpn/pptp"),   _("PPTP Client"), 30).leaf = true
 	entry({"admin", "services", "l2tp"},   cbi("vpn/l2tp"),   _("L2TP Client"),   40).leaf = true
 	entry({"admin", "services", "gre"},    cbi("vpn/gre"),    _("GRE Client"),    50).leaf = true
 	entry({"admin", "services", "dmvpn"},  cbi("vpn/dmvpn"),  _("DMVPN Tunnels"),  60).leaf = true
@@ -19,28 +22,44 @@ function index()
 
 end
 
-function do_pptp_action(action, con_id)
+function do_action(action, vpnType)
+	local name = luci.http.formvalue("name")
+	local isActive = luci.http.formvalue("isActive")
+	local options = luci.jsonc.parse(luci.http.formvalue("options"))
+	local config = 'gre_tunnels'
+
 	local commands = {
 		add= function(...)
+			uci:section(config, vpnType, name, options)
+			uci:set(config, name, 'isActive', 'false')
+			uci:commit(config)
 		end,
 
 		edit = function(...)
+			for key, value in pairs(options) do
+				uci:set(config, name, key, value)
+			end
+			uci:commit(config)
 		end,
 
 		delete = function(...)
+			uci:delete(config, name)
+			uci:commit(config)
 		end,
 
-		enable = function(...)
+		enable = function(vpnType)
+			uci:set(config, name, 'isActive', isActive)
+			uci:commit(config)
 		end,
 
 		default = function(...)
 			http.prepare_content("text/plain")
-			http.write("0")
+			http.write(vpnType .. " " .. name)
 		end
 	}
 
 	if commands[action] then
-		commands[action](con_id)
+		commands[action](vpnType)
 		commands["default"]()
 	end
 end
